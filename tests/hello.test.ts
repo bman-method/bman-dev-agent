4c6082ec5d5edda9ed39717f0ec8027c276323d3
yotammadem <yotammadem@gmail.com>
2025-12-13 01:33:07 +0200

    remove helloWorld function and its associated tests

--- tests/hello.test.ts
+++
@@ -1,7 +0,0 @@
-import { helloWorld } from "../src/index";
-
-describe("helloWorld", () => {
-  it("returns the expected greeting", () => {
-    expect(helloWorld()).toBe("Hello, world!");
-  });
-});
